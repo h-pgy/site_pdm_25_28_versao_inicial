abebb24dc47484b73d3f145ef5589d3e865d5f3d
h-pgy <hpougy@gmail.com>
2025-03-17 02:22:25 -0300

    ajustes cards programas anteriores

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -89,3 +89,20 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+function openModal(id) {
+    document.getElementById("modal-" + id).style.display = "flex";
+}
+
+function closeModal(id) {
+    document.getElementById("modal-" + id).style.display = "none";
+}
+
+window.onclick = function (event) {
+    document.querySelectorAll(".modal").forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+};
+
